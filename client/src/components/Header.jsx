fb051807ccaf8b0d5480a028498f6036be23f98a
Saksham Kasera <sakshamkaseraa@gmail.com>
2024-06-12 09:42:58 +0530

    create a test api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
           <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-          <Link to='/SignIn'>
+          <Link to='/sign-in'>
             <li className=' text-slate-700 hover:underline'>SignIn</li>
           </Link>
         </ul>
