194eeac86a1ccf14ca84b6babb4c68e5cd3f7a5f
Saksham Kasera <sakshamkaseraa@gmail.com>
2024-06-13 15:35:03 +0530

    signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorhandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next) => {
     const {username , email , password} = req.body;
@@ -9,6 +10,25 @@ export const signup = async (req,res,next) => {
         const newUser = new User({username,email,password : hashedPassowrd});
         await newUser.save();
         res.status(201).json('user created succesfully');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req,res,next) => {
+    const {username , password} = req.body;      
+    try {
+        const validUser = await User.findOne({username});
+        if(!validUser){
+            return next(errorhandler(404,'User Not Found'));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorhandler(401,'Wrong credentials'));
+        }
+        const token = jwt.sign({ id : validUser._id},process.env.secret);
+        const {password : pass , ...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly: true }).status(200).json(rest); 
     } catch (error) {
         next(error);
     }
