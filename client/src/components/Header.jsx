3259c80e20a2c6e781beac07a475614dea5984f3
Saksham Kasera <sakshamkaseraa@gmail.com>
2024-06-14 13:31:54 +0530

    redux persist

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
           <input type="text" placeholder='Search..' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
           <FaSearch className='text-slate-600'/>
         </form>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-6'>
           <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
           </Link>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key : 'root',
+    storage,
+    version : 1
+}
+
+const persistedReducer = persistReducer(persistConfig,userReducer)
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware : (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
 })
+
+export const persistor = persistStore(store);
