a07de61e7a148e40b53413d72ffbdeca890cb20c
Saksham Kasera <sakshamkaseraa@gmail.com>
2024-06-17 19:44:13 +0530

    upload image

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,68 +1,84 @@
-import React from 'react'
-import {Link, useNavigate} from 'react-router-dom';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
-
   const [formData, setFormData] = useState({});
-  const { loading, error} = useSelector((state) => state.user)
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id] : e.target.value,
+      [e.target.id]: e.target.value,
     });
   };
 
-  const handleSubmit =  async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
+    try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin',
-        {
-          method : 'POST',
-          headers:{
-            'Content-Type' : 'application/json'
-          },
-          body : JSON.stringify(formData)
-        }
-      );
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
       const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
       }
-      
-      dispatch(signInSuccess(data))
-      navigate('/');
 
-    }catch(error){
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
       dispatch(signInFailure(error.message));
     }
-
-  } 
+  };
 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password'onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-opacity-70'>{loading ? 'Loading' : 'Sign In'}</button>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-opacity-70"
+        >
+          {loading ? "Loading" : "Sign In"}
+        </button>
         <OAuth></OAuth>
-      </form >
-      <div className='flex gap-2 mt-4'>
+      </form>
+      <div className="flex gap-2 mt-4">
         <p>Dont have an account ?</p>
-        <Link to={'/sign-up'}>
-          <span className='text-blue-700'>Sign Up</span>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-4'>{error}</p>}
+      {error && <p className="text-red-500 mt-4">{error}</p>}
     </div>
-  )
+  );
 }
